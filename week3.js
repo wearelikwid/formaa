8e73521a73396f8db8ea29a558800c934aa85069
hussein <86411714+wearelikwid@users.noreply.github.com>
2024-11-14 13:19:23 +0200

    Create week3.js

---
+++ week3.js
@@ -0,0 +1,152 @@
+const week3 = {
+    upperBody: {
+        name: "Upper Body",
+        circuits: [
+            {
+                name: "Circuit 1",
+                rounds: 4,
+                exercises: [
+                    "Wide pull up x 10 reps",
+                    "Diamond push ups x 12 reps",
+                    "Ring/TRX row x 10 reps",
+                    "Straight bar dips x 12 reps",
+                    "Neutral grip pull up x 10 reps"
+                ]
+            },
+            {
+                name: "Circuit 2",
+                rounds: 4,
+                exercises: [
+                    "Chin ups x 8 reps",
+                    "Push ups x 15 reps",
+                    "Face pull x 12 reps",
+                    "Dips x 15 reps",
+                    "Mixed grip pull up x 8 reps (4 each grip)"
+                ]
+            },
+            {
+                name: "Circuit 3",
+                rounds: 4,
+                exercises: [
+                    "TRX T row x 10 reps",
+                    "TRX Y row x 10 reps",
+                    "TRX bicep curl x 10 reps",
+                    "TRX face pull x 10 reps"
+                ]
+            }
+        ]
+    },
+    lowerBody: {
+        name: "Lower Body",
+        circuits: [
+            {
+                name: "Circuit 1",
+                rounds: 3,
+                exercises: [
+                    "Sumo squat x 10 reps",
+                    "Sumo deadlift x 10 reps"
+                ]
+            },
+            {
+                name: "Circuit 2",
+                rounds: 3,
+                exercises: [
+                    "Back squats x 10 reps",
+                    "Walking lunges with bar 30kg (2 as 1) x 10 steps"
+                ]
+            },
+            {
+                name: "Circuit 3",
+                rounds: 3,
+                exercises: [
+                    "Hamstring curls machine/TRX x 15 reps OR Nordic hamstring curls x 10 reps",
+                    "Nordic quads x 10 reps"
+                ]
+            },
+            {
+                name: "Circuit 4",
+                rounds: 3,
+                exercises: [
+                    "Single leg calf raises (15kg) x 15 reps",
+                    "Achilles raises x 15 reps"
+                ]
+            },
+            {
+                name: "Circuit 5",
+                rounds: 3,
+                exercises: [
+                    "Split squat each side (8-10kg DBs) x 8 reps"
+                ]
+            }
+        ]
+    },
+    core: {
+        name: "Core",
+        note: "40 seconds on, 20 seconds off, 4 rounds each",
+        circuits: [
+            {
+                name: "Circuit 1",
+                exercises: [
+                    "High plank feet on box (+10kg)",
+                    "DB V-sit shoulder press",
+                    "TRX superman plank (hands on TRX)"
+                ]
+            },
+            {
+                name: "Circuit 2",
+                exercises: [
+                    "Hanging knee raises with wall ball/DB (3-5kg)",
+                    "KB Russian twists",
+                    "TRX High plank tuck pike alternating knee to elbow"
+                ]
+            },
+            {
+                name: "Circuit 3",
+                exercises: [
+                    "TRX low plank (+10kg)",
+                    "Hollow body hold",
+                    "Bicycle crunches"
+                ]
+            },
+            {
+                name: "Circuit 4",
+                exercises: [
+                    "Tuck pike situps",
+                    "Hollow rocks",
+                    "Back extensions"
+                ]
+            }
+        ]
+    },
+    fullBody: {
+        name: "Full Body",
+        circuits: [
+            {
+                name: "For Time Circuit 1",
+                type: "For Time",
+                note: "Alternate between exercises when resting",
+                exercises: [
+                    "Wall ball (9-10kg) x 150 reps",
+                    "KB swings (20-24kg) x 150 reps"
+                ]
+            },
+            {
+                name: "For Time Circuit 2",
+                type: "For Time",
+                note: "Alternate between exercises when resting",
+                exercises: [
+                    "KB thrusters (12kg) x 100 reps",
+                    "Chest to ground burpees x 100 reps"
+                ]
+            },
+            {
+                name: "For Time Circuit 3",
+                type: "For Time",
+                exercises: [
+                    "Slam ball (35lb) x 100 reps",
+                    "Side kick through (2 as 1) x 100 reps"
+                ]
+            }
+        ]
+    }
+};
