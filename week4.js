c8f367ff69763ffc4a40278ed272cf72bc23c939
hussein <86411714+wearelikwid@users.noreply.github.com>
2024-11-14 13:20:13 +0200

    Create week4.js

---
+++ week4.js
@@ -0,0 +1,154 @@
+const week4 = {
+    upperBody: {
+        name: "Upper Body",
+        note: "Complete 4 rounds of supersets",
+        circuits: [
+            {
+                name: "Superset 1",
+                rounds: 4,
+                exercises: [
+                    "Neutral pull up x 10 reps",
+                    "Dips x 12-15 reps"
+                ]
+            },
+            {
+                name: "Superset 2",
+                rounds: 4,
+                exercises: [
+                    "Row (TRX or barbell 50-60kg) x 12 reps",
+                    "Weighted pushups (+15kg) x 10 reps + max bodyweight"
+                ]
+            },
+            {
+                name: "Superset 3",
+                rounds: 4,
+                exercises: [
+                    "DB complex: Front raise x 10, Side raise x 10, Upright row x 10",
+                    "Cable Facepull x 12 reps"
+                ]
+            },
+            {
+                name: "Superset 4",
+                rounds: 4,
+                exercises: [
+                    "Wide pullups x 10 reps",
+                    "Decline push ups (legs on bench) x 15 reps"
+                ]
+            },
+            {
+                name: "Superset 5",
+                rounds: 4,
+                exercises: [
+                    "Chin ups x 10 reps",
+                    "DB shoulder press or Pike push ups x 10 reps"
+                ]
+            }
+        ]
+    },
+    lowerBody: {
+        name: "Lower Body",
+        circuits: [
+            {
+                name: "Circuit 1",
+                rounds: 3,
+                exercises: [
+                    "Goblet squat (12.5/16kg KB/DB) x 10 reps",
+                    "KB reverse lunges (1 is 1) x 10 reps"
+                ]
+            },
+            {
+                name: "Circuit 2",
+                rounds: 4,
+                exercises: [
+                    "Single leg hamstring curl machine x 10 reps",
+                    "Nordic quads x 10 reps"
+                ]
+            },
+            {
+                name: "Circuit 3",
+                rounds: 4,
+                note: "15,12,10,8 reps",
+                exercises: [
+                    "Back squat",
+                    "Romanian Deadlift"
+                ]
+            },
+            {
+                name: "Circuit 4",
+                rounds: 4,
+                exercises: [
+                    "Zercher squat x 10 reps",
+                    "Hip thrusters x 10 reps"
+                ]
+            },
+            {
+                name: "Circuit 5",
+                rounds: 4,
+                exercises: [
+                    "Step ups with bar x 8 reps each leg"
+                ]
+            },
+            {
+                name: "Circuit 6",
+                rounds: 4,
+                exercises: [
+                    "Single leg calf raises (15kg DB) x 12 reps"
+                ]
+            }
+        ]
+    },
+    core: {
+        name: "Core",
+        warmup: {
+            name: "Warm-up",
+            rounds: 2,
+            exercises: [
+                "Crunches x 40 reps",
+                "Side crunch right x 30 reps",
+                "Side crunch left x 30 reps",
+                "Bicycle crunches x 20 reps",
+                "Tuck situps x 20 reps",
+                "Leg raises x 20 reps",
+                "Reverse crunch (hip raises) x 20 reps",
+                "Low plank x 1:30 minutes"
+            ]
+        },
+        mainCircuit: {
+            name: "Main Circuit",
+            note: "40 seconds on, 20 seconds off, 4 rounds",
+            exercises: [
+                "Back extensions",
+                "V-ups",
+                "TRX low plank",
+                "Russian twists",
+                "L-sit",
+                "Leg raises on decline bench",
+                "Roll outs",
+                "Weighted situps with KB",
+                "TRX high plank tuck pikes",
+                "Hollow rocks"
+            ]
+        }
+    },
+    fullBody: {
+        name: "Full Body",
+        circuits: [
+            {
+                name: "8-minute circuit",
+                rounds: 4,
+                note: "40 sec work/20 sec rest",
+                exercises: [
+                    "Wall balls",
+                    "KB swings",
+                    "Burpee pull-ups",
+                    "KB squat clean",
+                    "Slam ball",
+                    "DB double snatch",
+                    "Row machine",
+                    "Side kick through"
+                ],
+                finisher: "Jump rope x 2 minutes"
+            }
+        ]
+    }
+};
