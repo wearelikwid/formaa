10de581f4600b436d9045e5a2f3b5ee8cff4f423
hussein <86411714+wearelikwid@users.noreply.github.com>
2024-11-14 14:18:13 +0200

    Update week13.js

--- week13.js
+++ week13.js
@@ -1,140 +1,285 @@
-const week13 = {
-    upperBody: {
-        name: "Upper Body",
-        circuits: [
+{
+  "week13": {
+    "upperBody": {
+      "name": "Upper Body",
+      "circuits": [
+        {
+          "name": "Circuit 1",
+          "rounds": 4,
+          "exercises": [
             {
-                name: "Circuit 1",
-                rounds: 4,
-                exercises: [
-                    "Archer pull-ups (5 each side) x 10 reps",
-                    "Ring push-ups x 12 reps"
-                ]
+              "name": "Archer pull-ups",
+              "reps": 10,
+              "note": "5 each side"
             },
             {
-                name: "Circuit 2",
-                rounds: 4,
-                exercises: [
-                    "L-sit pull-ups x 10 reps",
-                    "Pseudo planche push-ups x 15 reps"
-                ]
-            },
-            {
-                name: "Circuit 3",
-                rounds: 4,
-                exercises: [
-                    "Commando pull-ups (5 each side) x 10 reps",
-                    "Ring dips x 12 reps"
-                ]
-            },
-            {
-                name: "Circuit 4",
-                rounds: 4,
-                exercises: [
-                    "Inverted rows (+15kg) x 12 reps",
-                    "Pike push-ups x 15 reps",
-                    "Face pulls x 10 reps"
-                ]
+              "name": "Ring push-ups",
+              "reps": 12
             }
-        ]
+          ]
+        },
+        {
+          "name": "Circuit 2",
+          "rounds": 4,
+          "exercises": [
+            {
+              "name": "L-sit pull-ups",
+              "reps": 10
+            },
+            {
+              "name": "Pseudo planche push-ups",
+              "reps": 15
+            }
+          ]
+        },
+        {
+          "name": "Circuit 3",
+          "rounds": 4,
+          "exercises": [
+            {
+              "name": "Commando pull-ups",
+              "reps": 10,
+              "note": "5 each side"
+            },
+            {
+              "name": "Ring dips",
+              "reps": 12
+            }
+          ]
+        },
+        {
+          "name": "Circuit 4",
+          "rounds": 4,
+          "exercises": [
+            {
+              "name": "Inverted rows",
+              "reps": 12,
+              "weight": "+15kg"
+            },
+            {
+              "name": "Pike push-ups",
+              "reps": 15
+            },
+            {
+              "name": "Face pulls",
+              "reps": 10
+            }
+          ]
+        }
+      ]
     },
-    lowerBody: {
-        name: "Lower Body",
-        sections: [
+    "lowerBody": {
+      "name": "Lower Body",
+      "sections": [
+        {
+          "name": "Explosive Warmup",
+          "rounds": 2,
+          "exercises": [
             {
-                name: "Explosive Warmup",
-                rounds: 2,
-                exercises: [
-                    "Mountain climbers x 20 reps",
-                    "Jump rope high knees x 15 reps",
-                    "Burpee broad jumps x 10 reps"
-                ]
+              "name": "Mountain climbers",
+              "reps": 20
             },
             {
-                name: "Main Work",
-                rounds: 4,
-                exercises: [
-                    "Elevated single-leg squats x 12 each leg",
-                    "Explosive switch lunges x 15 reps",
-                    "Lateral lunge walks x 20 (10 each side)"
-                ]
+              "name": "Jump rope high knees",
+              "reps": 15
             },
             {
-                name: "Balance Sequence",
-                rounds: 3,
-                exercises: [
-                    "Single-leg stance with eyes closed x 30 sec each leg",
-                    "Airborne lunges x 10 each leg",
-                    "Curtsy lunges to side kicks x 15 each side"
-                ]
-            },
-            {
-                name: "Power Finisher",
-                rounds: 4,
-                exercises: [
-                    "Sprint in place x 30 sec",
-                    "Pogo jumps x 30 sec",
-                    "Scissor jumps x 30 sec",
-                    "Rest x 30 sec"
-                ]
+              "name": "Burpee broad jumps",
+              "reps": 10
             }
-        ]
+          ]
+        },
+        {
+          "name": "Main Work",
+          "rounds": 4,
+          "exercises": [
+            {
+              "name": "Elevated single-leg squats",
+              "reps": 12,
+              "note": "each leg"
+            },
+            {
+              "name": "Explosive switch lunges",
+              "reps": 15
+            },
+            {
+              "name": "Lateral lunge walks",
+              "reps": 20,
+              "note": "10 each side"
+            }
+          ]
+        },
+        {
+          "name": "Balance Sequence",
+          "rounds": 3,
+          "exercises": [
+            {
+              "name": "Single-leg stance with eyes closed",
+              "duration": "30 sec",
+              "note": "each leg"
+            },
+            {
+              "name": "Airborne lunges",
+              "reps": 10,
+              "note": "each leg"
+            },
+            {
+              "name": "Curtsy lunges to side kicks",
+              "reps": 15,
+              "note": "each side"
+            }
+          ]
+        },
+        {
+          "name": "Power Finisher",
+          "rounds": 4,
+          "exercises": [
+            {
+              "name": "Sprint in place",
+              "duration": "30 sec"
+            },
+            {
+              "name": "Pogo jumps",
+              "duration": "30 sec"
+            },
+            {
+              "name": "Scissor jumps",
+              "duration": "30 sec"
+            },
+            {
+              "name": "Rest",
+              "duration": "30 sec"
+            }
+          ]
+        }
+      ]
     },
-    core: {
-        name: "Core",
-        sections: [
+    "core": {
+      "name": "Core",
+      "sections": [
+        {
+          "name": "Circuit",
+          "description": "40 seconds work/20 seconds rest, 4 rounds",
+          "exercises": [
             {
-                name: "Circuit",
-                description: "40 seconds work/20 seconds rest, 4 rounds",
-                exercises: [
-                    "Renegade rows with push-up",
-                    "Turkish get-up position holds (alternate sides)",
-                    "Swiss ball jackknife to pike",
-                    "Bird dog crunch",
-                    "Ring fallout to body saw",
-                    "Hanging knee raise to twist",
-                    "Plank to downward dog walks"
-                ]
+              "name": "Renegade rows with push-up",
+              "work": "40 sec",
+              "rest": "20 sec"
             },
             {
-                name: "Finisher",
-                rounds: 3,
-                exercises: [
-                    "Medicine ball slams with rotation x 15 reps",
-                    "Mountain climbers x 20 each side",
-                    "Back extension with twist x 15 reps",
-                    "Hollow body rocks x 30 seconds"
-                ]
+              "name": "Turkish get-up position holds",
+              "work": "40 sec",
+              "rest": "20 sec",
+              "note": "alternate sides"
+            },
+            {
+              "name": "Swiss ball jackknife to pike",
+              "work": "40 sec",
+              "rest": "20 sec"
+            },
+            {
+              "name": "Bird dog crunch",
+              "work": "40 sec",
+              "rest": "20 sec"
+            },
+            {
+              "name": "Ring fallout to body saw",
+              "work": "40 sec",
+              "rest": "20 sec"
+            },
+            {
+              "name": "Hanging knee raise to twist",
+              "work": "40 sec",
+              "rest": "20 sec"
+            },
+            {
+              "name": "Plank to downward dog walks",
+              "work": "40 sec",
+              "rest": "20 sec"
             }
-        ]
+          ]
+        },
+        {
+          "name": "Finisher",
+          "rounds": 3,
+          "exercises": [
+            {
+              "name": "Medicine ball slams with rotation",
+              "reps": 15
+            },
+            {
+              "name": "Mountain climbers",
+              "reps": 20,
+              "note": "each side"
+            },
+            {
+              "name": "Back extension with twist",
+              "reps": 15
+            },
+            {
+              "name": "Hollow body rocks",
+              "duration": "30 seconds"
+            }
+          ]
+        }
+      ]
     },
-    fullBody: {
-        name: "Full Body",
-        sections: [
+    "fullBody": {
+      "name": "Full Body",
+      "sections": [
+        {
+          "name": "Every 2 minutes for 16 minutes",
+          "exercises": [
             {
-                name: "Every 2 minutes for 16 minutes",
-                exercises: [
-                    "Wall balls x 10 reps",
-                    "Kettlebell swings x 12 reps",
-                    "Burpees x 8 reps"
-                ]
+              "name": "Wall balls",
+              "reps": 10
             },
             {
-                name: "Circuit",
-                rounds: 3,
-                exercises: [
-                    "Dumbbell renegade rows x 12 reps",
-                    "Goblet squats x 15 reps",
-                    "Push press x 10 reps"
-                ]
+              "name": "Kettlebell swings",
+              "reps": 12
             },
             {
-                name: "Finisher",
-                exercises: [
-                    "Jump rope x 50 reps",
-                    "Russian twists x 20 reps",
-                    "Up-downs x 10 reps"
-                ]
+              "name": "Burpees",
+              "reps": 8
             }
-        ]
+          ]
+        },
+        {
+          "name": "Circuit",
+          "rounds": 3,
+          "exercises": [
+            {
+              "name": "Dumbbell renegade rows",
+              "reps": 12
+            },
+            {
+              "name": "Goblet squats",
+              "reps": 15
+            },
+            {
+              "name": "Push press",
+              "reps": 10
+            }
+          ]
+        },
+        {
+          "name": "Finisher",
+          "exercises": [
+            {
+              "name": "Jump rope",
+              "reps": 50
+            },
+            {
+              "name": "Russian twists",
+              "reps": 20
+            },
+            {
+              "name": "Up-downs",
+              "reps": 10
+            }
+          ]
+        }
+      ]
     }
-};
+  }
+}
